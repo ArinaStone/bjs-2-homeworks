bbfc720af7cce730943b5142f0b1b8cd5fc98a67
ArinaStone <Arina.Podkamennaya19@gmail.com>
2024-09-27 15:15:21 +0300

    Исправления

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-		arr = [];
-	} else if (d === 0) {
+	if (d === 0) {
 		arr = [-b / (2 * a)];
 	} else if (d > 0) {
 		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
@@ -17,6 +15,5 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let monthlyPercent = (percent / 100) / 12;
 	let mortgageBody = amount - contribution;
 	let monthlyPayment = mortgageBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
-	let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
-	return totalAmount;
+	return Number((monthlyPayment * countMonths).toFixed(2));
 }
